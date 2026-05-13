96bb0062bb961444a998b2d213d3788bfb399ff5
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2026-05-13 18:13:45 +0200

    pwa

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'wsp-cc-v1';
+const OFFLINE_URLS = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() =>
+        caches.match('/').then((r) => r || Response.error())
+      )
+    );
+  }
+});
